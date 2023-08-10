09fc938866e4393d169fed1b0a922067bcdce97a
Metsehafe Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-08-10 09:59:08 +0300

    Day-09 Tailwind task complete

--- Day-08/tailwind-task-recipe-app/public/index.js
+++ Day-08/tailwind-task-recipe-app/public/index.js
@@ -8,3 +8,22 @@ burger.addEventListener('click', () => {
     menu.classList.add('hidden');
   }
 })
+
+// Get all the recipe cards
+const recipeCards = document.querySelectorAll('.card');
+
+// Get all the recipe details divs
+const recipeDetails = document.querySelectorAll('.recipe-details');
+
+// Add click event listener to each recipe card
+recipeCards.forEach((card, index) => {
+  card.addEventListener('click', () => {
+    // Hide all recipe details divs
+    recipeDetails.forEach((detail) => {
+      detail.classList.add('hidden');
+    });
+
+    // Show the selected recipe details div
+    recipeDetails[index].classList.remove('hidden');
+  });
+});
